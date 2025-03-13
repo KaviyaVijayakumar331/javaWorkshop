13532b6ebfe4c3bbbec748015bc650825d396838
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-13 22:31:19 +0530

    Conditional operator

---
+++ Conditional operator/Condop.java
@@ -0,0 +1,10 @@
+public class Condop{
+    public static void main(String args[]){
+        System.out.println(34<12);
+        System.out.println(56>25);
+        System.out.println(56<=67);
+        System.out.println(45>=19);
+        System.out.println(10==10);
+        System.out.println(23!=56);
+    }
+}
