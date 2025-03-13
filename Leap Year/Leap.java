30492ef8213aadf7e80772449839f96629a713ce
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-13 23:27:44 +0530

    Finding Leap Year

---
+++ Leap Year/Leap.java
@@ -0,0 +1,12 @@
+public class Leap{
+    public static void main(String args[]){
+        int year=2020;
+        if(year%4==0)
+        {
+            System.out.println("Leap Year");
+        }
+        else{
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
