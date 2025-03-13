af3eeae637e2ece4b0994bd7c2cd7f1557f6d4f1
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-13 22:51:20 +0530

    Major Minor Finder

---
+++ MajorMinorFinder/Age.java
@@ -0,0 +1,13 @@
+public class Age{
+    public static void main(String args[]){
+        int age=27;
+        if(age>=18)
+        {
+            System.out.println("The Person is Major");
+        }
+        else
+        {
+            System.out.println("The Person is Minor");
+        }
+    }
+}
