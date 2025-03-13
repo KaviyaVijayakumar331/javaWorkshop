5d273a0076185711f65c8a456b521c646b6cd93e
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-13 23:14:05 +0530

    Positive Negative Finder

---
+++ PositiveNegativeFinder/Positive.java
@@ -0,0 +1,13 @@
+public class Positive{
+    public static void main(String args[]){
+        int num=49;
+        if(num>0)
+        {
+            System.out.println("The number is Positive");
+        }
+        else
+        {
+            System.out.println("The number is Negative");
+        }
+    }
+}
