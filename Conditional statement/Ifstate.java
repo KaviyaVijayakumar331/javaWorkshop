8d79573ad2373b653902ef82307fd2f25c012345
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-13 22:39:44 +0530

    If Statement

---
+++ Conditional statement/Ifstate.java
@@ -0,0 +1,12 @@
+public class Ifstate{
+    public static void main(String args[]){
+        if(true)
+        {
+            System.out.println("Program is correct");
+        }
+        if(false)
+        {
+            System.out.println("Program is Wrong");
+        }
+    }
+}
