a64ff8e92ec9db7b370431dfebadb171213cd983
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-14 22:00:23 +0530

    Cases

---
+++ Cases/Cases.java
@@ -0,0 +1,7 @@
+public class Cases{
+    public static void main(String args[]){
+        String name="Hello World";
+        System.out.println(name.toUpperCase());
+         System.out.println(name.toLowerCase());
+    }
+}
