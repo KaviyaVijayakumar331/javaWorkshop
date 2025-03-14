23b19943b7f2deea504b3816773f81636d408caa
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-14 20:52:36 +0530

    Printing N numbers

---
+++ N number/File1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class File1{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the num:");
+        int num=sc.nextInt();
+        int i=1;
+        while(i<=num){
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
