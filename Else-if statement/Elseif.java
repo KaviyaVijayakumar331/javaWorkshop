39fe082ae0e395170b995e946cf35790fe689b31
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-14 23:23:42 +0530

    Else-if statement

---
+++ Else-if statement/Elseif.java
@@ -0,0 +1,17 @@
+public class Elseif{
+    public static void main(String args[]){
+        int num=28;
+        if(num>0)
+        {
+            System.out.println("Positive");
+        }
+        else if(num==0)
+        {
+            System.out.println("Neutral");
+        }
+        else
+        {
+            System.out.println("Negative");
+        }
+    }
+}
