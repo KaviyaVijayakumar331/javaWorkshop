3837229b5ed5b9224e9435b6de4a0f79cb58930a
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-14 20:45:36 +0530

    While Loop

---
+++ While Loop/While.java
@@ -0,0 +1,11 @@
+public class While{
+    public static void main(String args[]){
+        int i=92;
+        while(i<=98)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
+
