a23a4b3ae8d24dbcaa2a7b9938194995893b8166
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-14 20:17:46 +0530

    Sum the number of digits

---
+++ Sumofdigits/Sum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Sum{
+    public static void main(String args[]){
+        int count=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the n values:");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int i=0;
+        int sum=0;
+        while(i<n){
+            System.out.print("Enter the value:");
+            a[i]=sc.nextInt();
+            i=i+1;
+        }
+        i=0;
+            while(i<n)
+            {
+                sum=sum+a[i];
+                i=i+1;
+
+            }
+            System.out.println("The sum is:"+sum);
+        }
+
+        
+    }
