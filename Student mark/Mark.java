a5a5e3d587d65b39e39059c3ecaac16eda36a665
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-15 22:13:49 +0530

    Student mark

---
+++ Student mark/Mark.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Mark{
+    public void calculateGrade(int mark)
+    {
+        if(mark>=90){
+            System.out.println("Grade A");
+        }
+            else if(mark>=75){
+                System.out.println("Grade B");
+            } 
+            else if(mark>=50){
+                System.out.println("Grade C");
+            }
+            else{
+                System.out.println("Grade F");
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        while(true){
+
+        System.out.println("mark:");
+        int mark1=sc.nextInt();
+        Mark obj=new Mark();
+        obj.calculateGrade(mark1);
+    }
+    }
+}
