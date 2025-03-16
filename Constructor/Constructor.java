a7795cb35bf41545e091f6a4237a665f08edbd67
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-16 21:14:46 +0530

    Constructor

---
+++ Constructor/Constructor.java
@@ -0,0 +1,9 @@
+public class Constructor{
+    Constructor(){
+        System.out.println("Hello");
+    }
+    public static void main(String args[]){
+        Constructor obj=new Constructor();
+
+    }
+}
