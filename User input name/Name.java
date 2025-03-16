4037aaadffb985cb7d563362d86fe49f54b2980a
kaviyavijayakumar331 <kaviya123friend@gmail.com>
2025-03-16 20:39:13 +0530

    Getting a input string from the user

---
+++ User input name/Name.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Name{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a Name:");
+        String name=input.nextLine();
+        System.out.println(name);
+    }
+}
